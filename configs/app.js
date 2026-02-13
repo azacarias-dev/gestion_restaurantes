a446b4c02020306fafa30ae4d29fbd865bfe2765
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2026-02-13 15:20:55 -0600

    Se cargo la estructura como la configuracion del proyecto para empezar con el trabajo

---
+++ configs/app.js
@@ -0,0 +1,53 @@
+'use strict';
+
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { corsOptions } from './cors-configuration.js';
+import { dbConnection } from './db.js';
+
+
+const BASE_URL = '/gestionRestaurantes/v1';
+
+const middlewares = (app) => {
+    app.use(express.urlencoded({ extended: false, limit: '10mb'}));
+    app.use(express.json({limit: '10mb'}));
+    app.use(cors(corsOptions));
+    app.use(morgan('dev'));
+}
+
+
+const routes = (app) => {
+    // Rutas de la aplicacion
+}
+
+const initServer = async (app) => {
+    app = express();
+    const PORT = process.env.PORT || 3001;
+
+    try {
+        dbConnection();
+        middlewares(app);
+        routes(app);
+        
+        app.listen(PORT, () => {
+            console.log(`Servidor corriendo en el puerto ${PORT}`);
+            console.log(`Base URL: http://localhost:${PORT}${BASE_URL}`)
+        });
+
+        app.get(`${BASE_URL}/health`, (req, res) => { 
+            res.status(200).json(
+                {
+                    status: 'ok',
+                    service: 'Gestion de restaurantes',
+                    version: '1.0.0'
+                }
+            );
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export  { initServer };
