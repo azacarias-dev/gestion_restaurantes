ddd225ab7e237a3b05feb8ed73587b8bfa90b8b6
FernanMaldonado <fmaldonado-2024021@kinal.edu.gt>
2026-02-13 19:47:54 -0600

    Se completaron rutas (Get, Post y Put) para Reservaciones

---
+++ src/Reservaciones/reservaciones.routes.js
@@ -0,0 +1,34 @@
+'use strict'
+
+import { Router } from 'express';
+import { getReservations, getReservationById, createReservation, updateReservation, changeReservationStatus } from './reservaciones.controller.js';
+import { validateCreateReservation, validateUpdateReservation, validateReservationStatusChange, validateGetReservationById } from '../../middlewares/reservation-validation.js';
+
+const router = Router();
+
+router.get('/', getReservations);
+router.get('/:id', validateGetReservationById, getReservationById);
+router.post('/', validateCreateReservation, createReservation);
+router.put('/:id', validateUpdateReservation, updateReservation);
+router.put('/:id/confirmar',
+    (req, res, next) => {
+        req.body = req.body || {};
+        req.body.estado = 'confirmada';
+        next();
+    },
+    validateReservationStatusChange,
+    changeReservationStatus
+);
+
+
+router.put('/:id/cancelar',
+    (req, res, next) => {
+        req.body = req.body || {};
+        req.body.estado = 'cancelada';
+        next();
+    },
+    validateReservationStatusChange,
+    changeReservationStatus
+);
+
+export default router;
