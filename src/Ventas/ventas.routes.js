b72d74a46d70b87ce4355576d3f402aaced3c54e
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2026-02-15 11:24:42 -0600

    Se crearon los validadores de ventas para capturar errores

--- src/Ventas/ventas.routes.js
+++ src/Ventas/ventas.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { crearVenta, getVentas, getVentaById } from "./ventas.controller.js";
+import { createVentaValidator, getVentaByIdValidator } from "../../middlewares/ventas-validators.js";
 
 const router = Router();
 
-router.post('/', crearVenta);
+router.post('/', createVentaValidator, crearVenta);
 router.get('/', getVentas);
-router.get('/:id', getVentaById);
+router.get('/:id', getVentaByIdValidator, getVentaById);
 export default router;
