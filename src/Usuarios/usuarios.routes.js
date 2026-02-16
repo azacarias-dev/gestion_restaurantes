425ed820079bf4342666fdddbce514041850b696
GianCordova <gcordova-2024016@kinal.edu.gt>
2026-02-16 08:01:18 -0600

    Se añadio el metodo get a el modulo de usuarios

--- src/Usuarios/usuarios.routes.js
+++ src/Usuarios/usuarios.routes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { createAccount, updateAccount } from './usuarios.controller.js';
+import { getUsers, getUserById ,createAccount, updateAccount } from './usuarios.controller.js';
 import { createAccountValidator } from '../../middlewares/usuarios-validator.js';
 
 const router = Router();
+
+router.get('/getUsers', getUsers);
+
+router.get('/getUserById/:id', getUserById);
 
 router.post('/createAccount', createAccountValidator, createAccount);
 
