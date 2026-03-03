2e8054fd6659af1138d17e6e1ba6b6d07e7b805d
JavierGomez0 <jgomez-2024026@kinal.edu.gt>
2026-03-03 17:55:13 -0600

    se termino el model de sucursales

---
+++ Administrador/src/Sucursal/sucursal.model.js
@@ -0,0 +1,46 @@
+'use strict';
+
+import mongoose from "mongoose";
+
+const sucursalSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre de la sucursal es obligatorio'],
+        maxlength: [100, 'El nombre no puede tener mas de 100 caracteres'],
+        trim: true,
+    },
+    direccion: {
+        type: String,
+        required: [true, 'La dirección es obligatoria'],
+        maxlength: [300, 'La dirección no puede tener mas de 300 caracteres'],
+        trim: true,
+    },
+    telefono: {
+        type: String,
+        required: [true, 'El teléfono es obligatorio'],
+        maxlength: [20, 'El teléfono no puede tener mas de 20 caracteres'],
+        trim: true,
+    },
+    horario: {
+        apertura: {
+            type: String,
+            required: [true, 'La hora de apertura es obligatoria'],
+        },
+        cierre: {
+            type: String,
+            required: [true, 'La hora de cierre es obligatoria'],
+        }
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+},
+{
+    timestamps: true
+});
+
+sucursalSchema.index({ nombre: 1 }, { unique: true });
+sucursalSchema.index({ isActive: 1, nombre: 1 });
+
+export default mongoose.model('Sucursales', sucursalSchema);
